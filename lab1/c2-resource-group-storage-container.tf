eba69a6f552ffaf16ae5954bf8685dcb778fcddd
nabeeljarral <nabeelazam33@gmail.com>
2026-06-01 08:21:37 +0500

    update resources: rename resource group and random string, change storage account environment to prod

--- lab1/c2-resource-group-storage-container.tf
+++ lab1/c2-resource-group-storage-container.tf
@@ -1,24 +1,24 @@
 # Resource-1: Azure Resource Group
-resource "azurerm_resource_group" "myrg1" {
+resource "azurerm_resource_group" "prodrg" {
   name = "myrg-1"
   location = "East US"
 }
 
 # Resource-2: Random String 
-resource "random_string" "myrandom" {
+resource "random_string" "prodrandom" {
   length = 16
   upper = false 
   special = false
 }
 
 # Resource-3: Azure Storage Account 
-resource "azurerm_storage_account" "mysa" {
-  name                     = "mysa${random_string.myrandom.id}"
-  resource_group_name      = azurerm_resource_group.myrg1.name
-  location                 = azurerm_resource_group.myrg1.location
+resource "azurerm_storage_account" "prodsa" {
+  name                     = "mysa${random_string.prodrandom.id}"
+  resource_group_name      = azurerm_resource_group.prodrg.name
+  location                 = azurerm_resource_group.prodrg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
   tags = {
-    environment = "staging"
+    environment = "prod"
   }
 }
