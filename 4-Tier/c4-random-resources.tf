46c48b2a60e3c216286254482bdf372ff5bf1ee9
nabeeljarral <nabeelazam33@gmail.com>
2026-06-03 13:00:55 +0500

    Refactor workflow and resource definitions for clarity and maintainability

--- 4-Tier/c4-random-resources.tf
+++ 4-Tier/c4-random-resources.tf
@@ -3,5 +3,10 @@ resource "random_string" "myrandom" {
   length = 6
   upper = false 
   special = false
-  numeric = false   
+  numeric = false 
+  keepers = {
+    # This ensures the random string regenerates only when these values change
+  resource_group_name = var.resource_group_name
+  environment         = var.environment
+  }  
 }
